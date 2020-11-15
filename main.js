40586beb4020b433222747a7ad64b1fcc96488c9
Rosano Coutinho <pub@xeari.com>
2020-11-15 12:57:38 -0300

    Define OLSKHotfixPatches

---
+++ main.js
@@ -0,0 +1,19 @@
+const mod = {
+
+	OLSKHotfixPatches () {
+		return {
+			'./node_modules/zombie/lib/document.js': [
+				[
+					'this.dispatchEvent(event);',
+					`this.dispatchEvent(event)
+						const handled = browser.emit('OLSKMessage', data);
+					  if (!handled)
+					      browser.log('Unhandled message("%s")');`
+				],
+			],
+		};
+	},
+
+};
+
+Object.assign(exports, mod);
