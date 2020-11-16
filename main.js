740e84674d7bc1b104d3fd43523da6b7e3f92541
Rosano Coutinho <pub@xeari.com>
2020-11-16 10:41:20 -0300

    Return patches based on environment

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 const mod = {
 
-	OLSKHotfixPatches () {
-		return {
+	OLSKHotfixPatches (environment) {
+		return Object.assign(Object.fromEntries(Object.entries({
 			'./node_modules/zombie/lib/document.js': {
-				'this.dispatchEvent(event);': `this.dispatchEvent(event)
-						const handled = browser.emit('OLSKMessage', data);
-					  if (!handled)
-					      browser.log('Unhandled message("%s")');`,
+				'this.dispatchEvent(event);': `[this.dispatchEvent(event), !browser.emit('OLSKMessage', data) && browser.log('Unhandled message("%s")')];`,
 			},
 			'./node_modules/ulid/dist/index.esm.js': {
 				'var allowInsecure = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;': "var allowInsecure = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : (typeof require === 'undefined' && typeof navigator !== 'undefined' && navigator.appName === 'Zombie');",
@@ -14,21 +11,23 @@ const mod = {
 				'export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory, ulid };':
 				'export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory };',
 			},
-			'./node_modules/launchlet/__compiled/launchlet.js': {
-				',100': ',1',
+			'./node_modules/simplecrypto/src/simplecrypto.js': {
+				'var _crypto = window.crypto || window.msCrypto;': 'var _crypto = window.crypto || window.msCrypto\nif (!_crypto) return;',
 			},
+		}).map(function ([path, patches]) {
+			return [path, Object.fromEntries(Object.entries(patches).map(function (e) {
+				return environment === 'production' ? e.reverse() : e;
+			}))];
+		})), {
 			'./node_modules/remotestoragejs/release/remotestorage.js': {
 				// 'options.redirectUri = globalContext.cordova ? config.cordovaRedirectUri : String(Authorize.getLocation());': 'options.redirectUri = globalContext.cordova ? config.cordovaRedirectUri : String(config.OLSKPatchRemoteStorageAuthRedirectURI || Authorize.getLocation());',
 				'e.redirectUri=m.cordova?l.cordovaRedirectUri:String(h.getLocation())': 'e.redirectUri=m.cordova?l.cordovaRedirectUri:String(l.OLSKPatchRemoteStorageAuthRedirectURI || h.getLocation())',
 				'r&&((t=u.getLocation()).hash': 'r.access_token&&((t=u.getLocation()).hash',
 			},
-			'./node_modules/simplecrypto/src/simplecrypto.js': {
-				'var _crypto = window.crypto || window.msCrypto;': `var _crypto = window.crypto || window.msCrypto
-			    if (!_crypto) {
-			        return
-			    };`,
-			},
-		};
+			'./node_modules/launchlet/__compiled/launchlet.js': {
+				',100': ',1',
+			},		
+		});
 	},
 
 };
